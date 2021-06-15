2b16529d52205f29e4421e05249ca2d5f8d79e36
Đông Trần Ngọc <57401513+ngocdong1999@users.noreply.github.com>
2021-06-15 17:48:55 +0700

    repair deploy

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,4 +1,4 @@
-export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : 'someDeployUrl';
+export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : 'https://warm-bayou-29593.herokuapp.com/api';
 export const LOCAL_STORAGE_TOKEN_NAME = 'todo-app';
 export const POSTS_LOADED_SUCCESS = 'POSTS_LOADED_SUCCESS';
 export const POSTS_LOADED_FAIL = 'POSTS_LOADED_FAIL';
